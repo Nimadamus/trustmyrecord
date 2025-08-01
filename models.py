d7cb5d0f7fd7ed9ccb01ae4bc78944d3fbd7fb7e
Nimadamus <nj2121@gmail.com>
2025-08-01 06:28:43 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,1 @@
-# File: models.py
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class User(db.Model):
-    # THIS IS THE FIX. SQLAlchemy requires this line.
-    id = db.Column(db.Integer, primary_key=True)
-
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+raise Exception("IT'S WORKING! WE ARE READING THE NEW FILE!")
