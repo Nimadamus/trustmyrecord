c952b412c455fba453da4c88be1d463e3981f457
Nimadamus <nj2121@gmail.com>
2025-08-01 06:25:25 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,27 +1,25 @@
+# File: models.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin # <--- IMPORT THIS
-from werkzeug.security import generate_password_hash, check_password_hash # <--- AND THESE
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-# Add UserMixin to the class definition
-class User(db.Model, UserMixin): # <--- ADD UserMixin HERE
-    """Represents a user in the database."""
-
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
+    # THIS IS THE CRITICAL LINE THAT FIXES THE ERROR
     id = db.Column(db.Integer, primary_key=True)
+
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    # We rename this to be clearer
-    password = db.Column(db.String(128), nullable=False) # <--- RENAMED FROM password_hash
+    password = db.Column(db.String(128), nullable=False)
 
-    # --- New Methods for setting and checking passwords ---
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    def set_password(self, password_to_hash):
+        self.password = generate_password_hash(password_to_hash)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    def check_password(self, password_to_check):
+        return check_password_hash(self.password, password_to_check)
 
     def __repr__(self):
-        return f'<User \'{self.username}\'>'
+        return f'<User {self.username}>'
