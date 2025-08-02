55c696f20427d373e67bb3db07c8e91122974f93
Nimadamus <nj2121@gmail.com>
2025-08-02 00:38:48 -0700

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,9 +1,7 @@
-# In extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-# Create the extension instances. Do not configure them here.
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
